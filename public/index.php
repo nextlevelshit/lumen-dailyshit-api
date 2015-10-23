b9e41f0ef5f64a26b945e40ebedee7ebfe5bd1db
nextlevelshit <mail@dailysh.it>
2015-10-23 22:22:17 +0200

    Added access controll to php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
 
 /*
 |--------------------------------------------------------------------------
